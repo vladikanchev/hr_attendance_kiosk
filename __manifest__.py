8f80022960a45a40659747743d14dff87bccb778
Vladimir Kanchev <95029070+vladikanchev@users.noreply.github.com>
2025-06-04 12:57:07 +0300

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -1,9 +1,9 @@
 {
-    "name": "HR Attendance Kiosk Limit Override",
+    "name": "HR Attendance Kiosk Limit Override (Fixed)",
     "version": "1.0",
-    "summary": "Ограничение на чек-ин и чек-аут в Kiosk Mode според час",
+    "summary": "Ограничение на чек-ин и чек-аут в Kiosk Mode според час (фиксирана версия)",
     "category": "Human Resources",
-    "author": "Teolino",
+    "author": "Teolino-Vladi",
     "depends": ["hr_attendance"],
     "data": [
         "views/kiosk_assets.xml"
