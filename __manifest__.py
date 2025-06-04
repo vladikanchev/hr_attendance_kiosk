dbd6afd6ee08765aaece11934bcbe47ff4956538
Vladimir Kanchev <95029070+vladikanchev@users.noreply.github.com>
2025-06-04 12:33:02 +0300

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "HR Attendance Kiosk Limit Override",
+    "version": "1.0",
+    "summary": "Ограничение на чек-ин и чек-аут в Kiosk Mode според час",
+    "category": "Human Resources",
+    "author": "Teolino",
+    "depends": ["hr_attendance"],
+    "data": [
+        "views/kiosk_assets.xml"
+    ],
+    "installable": True,
+    "application": False
+}
